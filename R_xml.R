989659431cce244a2ce876c4c2160ce0da779375
Estevan Bruginski <estevan.bruginski@gmail.com>
2021-08-30 15:51:51 -0300

    Add comentarios

--- R_xml.R
+++ R_xml.R
@@ -1,48 +1,55 @@
+# bibliotecas
 library(xml2)
 library(magrittr)
+# tempo inicial
 start_time <- Sys.time()
-# the contents of sample.xml are parsed
-hmdb_metabolites <- read_xml('data/hmdb/serum_metabolites.xml');
-
+# importar e fazer o parse do arquivo .xml
+hmdb_metabolites <- read_xml('serum_metabolites.xml');
+# extrair os nodes do arquivo xml
 met.nodes <- xml_find_all(hmdb_metabolites, './/d1:metabolite');
-
+# definir os campos que serão extraídos
 xpath_child.v <- c('./d1:accession',
                    './d1:name',
                    './d1:kegg_id',
                    './d1:average_molecular_weight',
                    './d1:chemical_formula');
-#what names should they get in the list?
-child.names.v <- c('CPD_NAME','KEGG_ID','MOL_WEIGHT', 'CHEM_FORM');
-
+# nomes que serão colocados na lista 
+child.names.v <- c('CPD_NAME',
+                   'KEGG_ID',
+                   'MOL_WEIGHT',
+                   'CHEM_FORM');
+# limpar a memória
 gc()
-
+# criar o espaço na memória
 hmdb.cpd <- NULL
+# barra de progresso
 pb <- txtProgressBar(0, length(met.nodes), style = 3)
-
+# loop para a extração dos dados
 for (i in 1:length(met.nodes)){
   setTxtProgressBar(pb, i)
-  #first, loop over the met.nodes
+  #primeiro loop nos nodes primários
   hmdb.cpd[i] <- lapply(met.nodes[i], function(x) { 
-  #second, loop over the xpath desired child-nodes
+  #segundo, loop para a seleção das informações(child-nodes)
   temp <- lapply(xpath_child.v, function(y) { 
     xml_find_all(x,y) %>% xml_text() %>% data.frame(value = .)
   });
-  #set their names
+  #definir os nomes
   names(temp) = child.names.v;
   return(temp);
 });
 }
 close(pb)
+# tempo final
 end_time <- Sys.time()
+# tempo total
 total_tim <- end_time - start_time
-
-
+# transforma a lista em dataframe
 hmdb.csv<- data.frame(matrix(unlist(hmdb.cpd), nrow=length(hmdb.cpd), byrow=TRUE));
-
+# define o nome das colunas
 colnames(hmdb.csv) <- c('HMDB_ID', 
                        'CPD_NAME', 
                        'KEGG_ID', 
                        'MOL_WEIGHT', 
                        'CHEM_FORM');
-
+# grava os dados em um arquivo de texto .csv
 write.table(hmdb.csv, 'data/hmdb.csv', row.names = FALSE, quote = FALSE, sep = ';')
